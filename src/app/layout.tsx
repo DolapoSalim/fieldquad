d3427103a9bb3d12d3c4c4cfd69d217f4c751bbd
Olatoye Dolapo Salim <dolaposalim@gmail.com>
2025-05-09 10:49:39 +0000

    Allow the text within the buttons to fit, also I wanna use the roboto as default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
+
 import type {Metadata} from 'next';
-import {Geist, Geist_Mono} from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  style: ['normal', 'italic'],
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${roboto.variable} font-sans antialiased`}>
         {children}
         <Toaster />
       </body>
